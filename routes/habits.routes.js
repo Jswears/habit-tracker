2a71cfcd360b31893febc219bba8dc05316ae6c5
Jswears <ji.swearssalinas@gmail.com>
2023-07-18 14:29:15 +0200

    adding habitsroutes and creating dinamyc update-form

---
+++ routes/habits.routes.js
@@ -0,0 +1,179 @@
+const router = require("express").Router();
+const { isLoggedIn } = require("../middleware/route-guard.js");
+const Habit = require("../models/Habits.model");
+const DailyHabit = require("../models/Daily.model");
+const TodoItem = require("../models/Todo.model");
+
+// CREATE HABIT
+router.post("/habits/create", isLoggedIn, async (req, res, next) => {
+  const { name } = req.body;
+  const currentUser = req.session.currentUser;
+
+  try {
+    const newHabit = await Habit.create({
+      name: name,
+      user: currentUser._id,
+    });
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// EDIT HABIT
+router.get("/habits/:id/edit", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const foundHabit = await Habit.findById(id);
+    res.render("updateForms/update-form", { foundHabit: foundHabit });
+  } catch (error) {
+    console.log("There has been an error: ", error);
+    next(error);
+  }
+});
+
+router.post("/habits/:id/edit", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const updatedHabit = await Habit.findByIdAndUpdate(
+      id,
+      {
+        name: name,
+      },
+      { new: true }
+    );
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// DELETE HABIT
+router.post("/habits/:id/delete", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await Habit.findByIdAndDelete(id);
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// CREATE DAILY HABIT
+router.post("/daily/create", isLoggedIn, async (req, res, next) => {
+  const { name } = req.body;
+  const currentUser = req.session.currentUser;
+
+  try {
+    const newDailyHabit = await DailyHabit.create({
+      name: name,
+      user: currentUser._id,
+    });
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// EDIT DAILY HABIT
+router.get("/daily/:id/edit", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const foundDaily = await DailyHabit.findById(id);
+    res.render("updateForms/update-form", { foundDaily: foundDaily });
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+router.post("/daily/:id/edit", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const updatedDailyHabit = await DailyHabit.findByIdAndUpdate(
+      id,
+      {
+        name: name,
+      },
+      { new: true }
+    );
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// DELETE DAILY HABIT
+router.post("/daily/:id/delete", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await DailyHabit.findByIdAndDelete(id);
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// CREATE TODO ITEM
+router.post("/todo/create", isLoggedIn, async (req, res, next) => {
+  const { name } = req.body;
+  const currentUser = req.session.currentUser;
+
+  try {
+    const newTodoItem = await TodoItem.create({
+      name: name,
+      user: currentUser._id,
+    });
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// EDIT TODO ITEM
+router.get("/todo/:id/edit", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const foundTodo = await TodoItem.findById(id);
+    res.render("updateForms/update-form", { foundTodo: foundTodo });
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+router.post("/todo/:id/edit", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const updatedTodoItem = await TodoItem.findByIdAndUpdate(
+      id,
+      {
+        name: name,
+      },
+      { new: true }
+    );
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+// DELETE TODO ITEM
+router.post("/todo/:id/delete", isLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await TodoItem.findByIdAndDelete(id);
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
+
+module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -27,8 +27,8 @@ router.get("/userProfile", isLoggedIn, async (req, res, next) => {
     const currentUser = req.session.currentUser;
 
     const habitList = await Habit.find({ user: currentUser._id });
-    const dailyHabits = await DailyHabit.find();
-    const todoItems = await TodoItem.find();
+    const dailyHabits = await DailyHabit.find({ user: currentUser._id });
+    const todoItems = await TodoItem.find({ user: currentUser._id });
 
     res.render("users/user-profile", {
       userInSession: req.session.currentUser,
@@ -36,101 +36,6 @@ router.get("/userProfile", isLoggedIn, async (req, res, next) => {
       dailyHabits,
       todoItems,
     });
-  } catch (error) {
-    console.log("There has been an error: ", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-//POST from userProfile
-router.post("/userProfile", isLoggedIn, async (req, res, next) => {
-  try {
-    //Create new Habit
-    const currentUser = req.session.currentUser;
-
-    // console.log(currentUser);
-    // console.log(req.body);
-    const { name } = req.body;
-    const newHabit = await Habit.create({
-      name: name,
-      user: currentUser._id,
-    });
-    res.redirect("/userProfile");
-  } catch (error) {
-    console.log("There has been an error: ", error);
-  }
-});
-
-//DELETE HABIT
-router.post("/userProfile/:id/delete", isLoggedIn, async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    await Habit.findByIdAndDelete(id);
-    await DailyHabit.findByIdAndDelete(id);
-    await TodoItem.findByIdAndDelete(id);
-    res.redirect("/userProfile");
-  } catch (error) {
-    console.log("There has been an error: ", error);
-  }
-});
-
-//EDIT HABIT
-router.get("/userProfile/:id/edit", isLoggedIn, async (req, res, next) => {
-  const { id } = req.params;
-
-  const foundHabit = await Habit.findById(id);
-  const foundDaily = await DailyHabit.findById(id);
-  const foundTodo = await TodoItem.findById(id);
-  res.render("habits/update-form", { foundHabit, foundDaily, foundTodo });
-});
-
-router.post("/userProfile/:id/edit", isLoggedIn, async (req, res, next) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  try {
-    const updatedHabit = await Habit.findByIdAndUpdate(
-      id,
-      {
-        name: name,
-      },
-      { new: true }
-    );
-    res.redirect("/userProfile");
-  } catch (error) {
-    console.log("There has been an error: ", error);
-  }
-});
-
-// POST to add a daily habit
-router.post("/userProfile/add-daily", isLoggedIn, async (req, res, next) => {
-  try {
-    const { name } = req.body;
-    const currentUser = req.session.currentUser;
-
-    const newDailyHabit = await DailyHabit.create({
-      name,
-      user: currentUser._id,
-    });
-
-    res.redirect("/userProfile");
-  } catch (error) {
-    console.log("There has been an error: ", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-//GET edit view
-
-// POST to add a todo item
-router.post("/userProfile/add-todo", isLoggedIn, async (req, res, next) => {
-  try {
-    const { name } = req.body;
-    const currentUser = req.session.currentUser;
-
-    const newTodoItem = await TodoItem.create({ name, user: currentUser._id });
-
-    res.redirect("/userProfile");
   } catch (error) {
     console.log("There has been an error: ", error);
     res.status(500).send("Internal Server Error");
